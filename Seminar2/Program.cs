f7bfe8e507bbb817850ee60f3663402a47b5c3e6
Vladislav <vlad.gorlach.2013@gmail.com>
2023-02-18 13:57:34 +0300

    Added tasks2

---
+++ Seminar2/Program.cs
@@ -0,0 +1,14 @@
+﻿int cutNumber(int num) // 354
+{
+    int hundreds = num / 10;
+    int units = num % 10;
+    int result = units;
+
+    return result;
+}
+
+int randNumber = new Random().Next(100,1000);
+
+int newNumber = cutNumber(randNumber);
+
+Console.WriteLine($"New wersian of {randNumber} is {newNumber}");

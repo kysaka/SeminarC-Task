462df660348ba0818cd11e4dc46176f3276aace3
Vladislav <vlad.gorlach.2013@gmail.com>
2023-02-18 18:35:45 +0300

    Added Task 2.3

--- Seminar2/Program.cs
+++ Seminar2/Program.cs
@@ -25,43 +25,76 @@
 // 78 -> третьей цифры нет
 // 32679 -> 6
 
-Console.Write("Enter a number: ");
-string number = (Console.ReadLine())!;
+/* -----------------------------------
+// Console.Write("Enter a number: ");
+// string number = (Console.ReadLine())!;
+// int num = Convert.ToInt32(number);
 
-if (number.Length == 3)
-{   
-    int num = Convert.ToInt32(number);
-    int units = num % 10;
-    int result = units;
-    Console.WriteLine($"The last digit: {result}");
-}
-else
-{   
-    if (number.Length > 3 && number.Length == 6)
-    {
-        int num = Convert.ToInt32(number);
-        int hundreds = num / 1000;
-        int units = hundreds % 10;
-        int result = units;
-        Console.WriteLine($"The last digit: {result}");
-    }
-    else
-    {
-        if (number.Length < 3 && number.Length != 6)
-        Console.WriteLine("Enter a three-digit or six-digit number ");
-    }
-}
+// if (number.Length > 3 && number.Length)
+//     {   
+//         int hundreds = num / 10;
+//         int units = hundreds % 10;
+//         int result = units;
+//         Console.WriteLine($"The last digit: {result}");
+//     }
+//     if (number.Length == 3)
+//     {   
+//         int units = num % 10;
+//         int result = units;
+//         Console.WriteLine($"The last digit: {result}");
+//    }
+-------------------------------------------------------------------*/
 
+// int number = ReadInt("Enter a number: ");
+// int count = number.ToString().Length;
+// Console.Write(MakeArray(number, count));
 
-// Console.Write("Input a nuber: ");
-// string input = Console.ReadLine();
-// if (input.Length == 3)
+// int ReadInt(string message)
 // {
-//     int number = Convert.ToInt32(input);
-//     int lastDigit = number % 10;
-//     Console.WriteLine($"The last digit: {lastDigit}");
+//     Console.Write(message);
+//     return Convert.ToInt32(Console.ReadLine());
 // }
-// else{
-//     Console.WriteLine("Error! Enter a three-digit number.");
-// };
 
+// int MakeArray(int number, int count)
+// {
+// int result = 0;
+//     if (count < 3)
+//     {
+//         Console.Write("There is no third digit, hold it: ");
+//     }
+//     else
+//     {
+//         int current = 1;
+//         for (int i = count; i > 3; i--)
+//         {
+//             current = current * 10;
+//         }
+
+//         result = (number / current) % 10;
+//     }
+// return result;
+// }
+
+// Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+// 6 -> да
+// 7 -> да
+// 1 -> нет
+
+// bool weekDay(int num)
+// {
+//         return (num == 6 || num == 7);
+// }
+
+// Console.Write("Enter the day of the week: "); 
+// int num = Convert.ToInt32(Console.ReadLine());
+
+// bool result = weekDay(num);
+
+// if(result)
+// {
+//     Console.WriteLine("This day is a day off ");
+// }
+// else
+// {
+//     Console.WriteLine("This day is not a day off ");
+// }

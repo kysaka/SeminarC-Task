ed872331d2226eac0416662e0aba52bcf4a10475
Vladislav <vlad.gorlach.2013@gmail.com>
2023-02-18 15:09:25 +0300

    Added tasks2.1

--- Seminar2/Program.cs
+++ Seminar2/Program.cs
@@ -17,3 +17,51 @@
 // int newNumber = cutNumber(randNumber);
 
 // Console.WriteLine($"New wersian of {randNumber} is {newNumber}");
+
+// ------------------------------------------------------------------
+
+// Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
+
+Console.Write("Enter a number: ");
+string number = (Console.ReadLine())!;
+
+if (number.Length == 3)
+{   
+    int num = Convert.ToInt32(number);
+    int units = num % 10;
+    int result = units;
+    Console.WriteLine($"The last digit: {result}");
+}
+else
+{   
+    if (number.Length > 3 && number.Length == 6)
+    {
+        int num = Convert.ToInt32(number);
+        int hundreds = num / 1000;
+        int units = hundreds % 10;
+        int result = units;
+        Console.WriteLine($"The last digit: {result}");
+    }
+    else
+    {
+        if (number.Length < 3 && number.Length != 6)
+        Console.WriteLine("Enter a three-digit or six-digit number ");
+    }
+}
+
+
+// Console.Write("Input a nuber: ");
+// string input = Console.ReadLine();
+// if (input.Length == 3)
+// {
+//     int number = Convert.ToInt32(input);
+//     int lastDigit = number % 10;
+//     Console.WriteLine($"The last digit: {lastDigit}");
+// }
+// else{
+//     Console.WriteLine("Error! Enter a three-digit number.");
+// };
+
